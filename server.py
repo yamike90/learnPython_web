d31b50bfc74f9e53919dc9a5ed338247a121a008
Mike Yampolsky <mike.yamp@gmail.com>
2019-10-09 17:08:19 +0300

    представление вынесено в шаблон

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    title = "Прогноз погоды"
     weather = weather_by_city("Moscow,Russia")
-    if weather:
-        return f"Температура: {weather['temp_C']}, ощущается как: {weather['FeelsLikeC']}."
-    else:
-        return "Сервис погоды временно недоступен."
+    return render_template('index.html', page_title=title, weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -9,17 +9,22 @@ def weather_by_city(city_name):
         "num_of_days" : 1,
         "lang" : "ru"
     }
-    result = requests.get(weather_url, params=params)
-    weather = result.json()
 
-    if 'data' in weather:
-        if "current_condition" in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status() # обработка ошибок 4** и 5**
+        weather = result.json()
+
+        if 'data' in weather:
+            if "current_condition" in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print("Сетевая ошибка")
+        return False
     return False
 
 if __name__ == "__main__":
-    w = weather_by_city("Moscow,Russia")
-    print(w)
+    print(weather_by_city("Moscow,Russia"))
