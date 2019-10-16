ae3fc154e35b652e445af91301eaab88a2fca14a
Mike Yampolsky <mike.yamp@gmail.com>
2019-10-16 22:36:57 +0300

    рефакторинг

--- webapp/weather.py
+++ webapp/weather.py
@@ -1,9 +1,10 @@
 import requests
+from flask import current_app
 
 def weather_by_city(city_name):
-    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    weather_url = current_app.config["WEATHER_URL"]
     params = {
-        "key" : "ec067db61d65471585c184536190210",
+        "key" : current_app.config["WEATHER_API_KEY"],
         "q" : city_name,
         "format" : "json",
         "num_of_days" : 1,
